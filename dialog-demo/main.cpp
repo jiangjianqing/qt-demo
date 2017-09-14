062a0651e7418efbdb2d614a7b2c7ecfbeaecd36
jjq <cz_jjq@163.com>
2017-09-14 08:13:48 +0800

    upgrade

--- dialog-demo/main.cpp
+++ dialog-demo/main.cpp
@@ -19,8 +19,8 @@
     #include <vld.h>
 #endif
 */
-//重要：在windows下才进行vld内存泄露测试
-#if defined(Q_OS_WIN32)
+//重要：在windows下并使用vc编译器才使用vld进行内存泄露测试
+#if defined(Q_OS_WIN32) && defined(Q_CC_MSVC)
     #include <vld.h>
     //abort(); // trap; generates core dump
 #else
