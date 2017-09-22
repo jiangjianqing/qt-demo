92c5247e9ff85c1508e23684366f19b845ad6ca0
jjq <cz_jjq@163.com>
2017-09-22 17:12:45 +0800

    upgrade

--- halcon-qt-demo/mainwindow.cpp
+++ halcon-qt-demo/mainwindow.cpp
@@ -12,3 +12,25 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btnLoadImage_clicked()
+{
+    HImage img;
+    HTuple hv_WindowID;
+    ReadImage(&img, "c:/image1.jpg");
+    Hlong width, height;
+    img.GetImageSize(&width, &height);
+
+    //在qt界面中显示HImage
+    Hlong winID=(Hlong)this->winId();//添加位置qt混合添加段
+    OpenWindow(0, 0, 0.5*width, 0.5*height,winID,"","",&hv_WindowID);//修改这句 添加winID
+    //OpenWindow(0,0,hv_Width/7,hv_Height/7,winID,"","",&hv_WindowID);//修改这句 添加winID
+    HDevWindowStack::Push(hv_WindowID);
+    if (HDevWindowStack::IsOpen())
+        DispObj(img, HDevWindowStack::GetActive());
+    //HWindow w(0, 0, 0.5*width, 0.5*height);
+    //img.DispImage(w);
+    //DispObj(img,ui->graphicsView);
+    //w.Click();
+    //w.ClearWindow();
+}
--- halcon-qt-demo/mainwindow.h
+++ halcon-qt-demo/mainwindow.h
@@ -3,6 +3,10 @@
 
 #include <QMainWindow>
 
+#include "HalconCpp.h"
+
+using namespace HalconCpp;
+
 namespace Ui {
 class MainWindow;
 }
@@ -14,6 +18,9 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+private slots:
+    void on_btnLoadImage_clicked();
 
 private:
     Ui::MainWindow *ui;
