805e8137ff59b456c102eb5343f4d4116ba9b004
jjq <cz_jjq@163.com>
2017-09-22 16:48:23 +0800

    upgrade

--- halcon-qt-demo/main.cpp
+++ halcon-qt-demo/main.cpp
@@ -1,6 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+//重要：在windows下并使用vc编译器才使用vld进行内存泄露测试
+#if defined(Q_OS_WIN32) && defined(Q_CC_MSVC) && defined(_DEBUG)
+    #include <vld.h>
+    //abort(); // trap; generates core dump
+#else
+    //exit(1); // goodbye cruel world
+
+#endif
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
