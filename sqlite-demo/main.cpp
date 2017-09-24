db0981133b84f4775a4e5b21a5f33256b15bf17c
cz_jjq <cz_jjq@163.com>
2017-09-24 09:12:28 +0800

    upgrade

--- sqlite-demo/main.cpp
+++ sqlite-demo/main.cpp
@@ -14,14 +14,28 @@ int main(int argc, char *argv[])
 
     StringUtil::doExample();
 
-    Task<int ()> task([]{ return 128128;});
+    Task<int ()> task([]{ return 32;});
 
     auto r1 = task.then([](int result){
         qDebug()<<"first result:"<<result;
-        return result+3;
+        return result + 3;
+    }).then([](int result)->int{
+        qDebug()<<"second result:"<<result;
+        return result + 3;
     }).get();
 
     qDebug() << r1 <<endl;
+
+    Task<int(int)> t1([](int i)->int{
+        return i;
+    });
+
+    t1.then([](int result)->std::string{
+        return std::to_string(result);
+    }).then([](const std::string& str)->int{
+        qDebug()<<str.c_str();
+       return 0;
+    }).get(1);
 
     return a.exec();
 }
